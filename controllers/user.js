0fb62af21b136dc707b17cdd433d12e74be2eac1
CoderGuy614 <jlutz.110@gmail.com>
2020-07-30 15:20:00 +0800

    Get User By Id Working

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,13 @@
 const User = require("../models/user");
 
 exports.read = (req, res) => {
-  return res.json(req.profile);
+  return res.json(req.user);
 };
 
 exports.userById = async (req, res, next, id) => {
   try {
     const user = await User.findById(id).select("-password");
-    req.profile = user;
+    req.user = user;
     next();
   } catch (error) {
     console.log(error);
